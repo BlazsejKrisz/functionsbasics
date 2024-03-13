e1f780a80b6506fbe056e293856d7bfd57c3c2c3
BlazsejKrisz <b.krisz4@gmail.com>
2024-03-13 10:30:40 +0100

    input a

--- events.js
+++ events.js
@@ -1,6 +1,7 @@
 const buttonA = document.querySelector("#button-a");
 const buttonB = document.querySelector("#button-b");
 const result = document.querySelector("#result");
+const inputA = document.querySelector("#text")
 
 function print(message) {
   const ts = Date.now();
@@ -10,15 +11,19 @@ function print(message) {
 }
 
 function handleButtonAClickEvent() {
-  print("Btton A Clicked");
+  print("Button A Clicked");
 }
 
 function handleButtonBClickEvent() {
   print("Button B Clicked");
 }
+function handleInputAInputEvent() {
+  print("Input A type");
+}
 
 buttonA.addEventListener("click", handleButtonAClickEvent);
 buttonB.addEventListener("click", handleButtonBClickEvent);
+inputA.addEventListener("input", handleInputAInputEvent)
 
 window.addEventListener("load", () => {
   print("Window loaded");
